f0ad3cd87314487d091cff33d8d762e9da2d5f4e
Nagababu1810 <nagababu.tester18@gmail.com>
2020-10-30 15:27:24 +0530

    cucumber

---
+++ cucmber.maven/src/main/java/logincucmber/Testrun.java
@@ -0,0 +1,14 @@
+package logincucmber;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class) 
+@CucumberOptions(features="Feature")
+
+public class Testrun {
+
+}
+//,glue={"scripting"}
